a2d7aaecba2433a3ca2a5a1d41c5e04f62ed0f85
Michał Łaszczewski <m8@em8.pl>
2019-05-06 08:19:14 +0200

    code

---
+++ index.js
@@ -0,0 +1,128 @@
+const r = require.main.rethinkdb || require('rethinkdb')
+if (require.main === module) require.main.rethinkdb = r
+
+const rtcms = require("../../RTCms")
+
+const users = rtcms.createServiceDefinition({
+  name: "users",
+  eventSourcing: true
+})
+
+const User = users.model({
+  name: "User",
+  properties: {
+    display: {
+      type: String
+    },
+    roles: {
+      type: Array,
+      of: {
+        type: String
+      }
+    },
+    loginMethods: {
+      type: Array,
+      of: {
+        type: Object
+      },
+      defaultValue: []
+    }
+  },
+  crud: {
+    deleteTrigger: true,
+    options: {
+      access: (params, {client, service}) => {
+        console.log("CHECK ACCESS", client)
+        return client.roles.includes('admin')
+      }
+    }
+  },
+  onChange(id, oldValue, newValue) {
+    //console.log("USER CHANGE", id, oldValue, newValue)
+    if((oldValue && newValue && JSON.stringify(oldValue.loginMethods) != JSON.stringify(newValue.loginMethods))
+       || (!oldValue && newValue)) {
+      let display = "unknown"
+      for(let loginMethod of newValue.loginMethods) {
+        if(loginMethod.type == 'emailPassword') display = loginMethod.email
+      }
+      //console.log("DISPLAY CHANGE", newValue.display, display)
+      if(display == newValue.display) {
+        //console.log("NO DISPLAY CHANGE NEEDED")
+        return Promise.resolve(true)
+      }
+      return User.update(id, { display })
+    }
+  }
+})
+
+
+users.action({
+  name: "UserUpdate", // override CRUD operation
+  properties: {
+    user: {
+      type: User,
+      idOnly: true
+    },
+    roles: {
+      type: Array,
+      of: {
+        type: String
+      }
+    }
+  },
+  returns: {
+    type: User,
+    idOnly: true
+  },
+  async execute({ user, roles }, context, emit) {
+    const userRow = await User.get(user)
+    if(!userRow) throw new Error("notFound")
+    emit([{
+      type: "UserUpdated",
+      data: {
+        roles
+      }
+    }])
+    emit("session" [{
+      type: "rolesUpdated",
+      user,
+      roles,
+      oldRoles : userRow.roles || []
+    }])
+    return emailPassword
+  }
+})
+
+
+users.event({
+  name: "loginMethodAdded",
+  async execute({ user, method }) {
+    console.log("LOGIN METHOD ADDED!", method)
+    await User.update(user, userRow => ({
+      loginMethods: userRow('loginMethods').append(method)
+    }))
+  }
+})
+
+users.event({
+  name: "loginMethodRemoved",
+  async execute({ user, method }) {
+    console.log("LOGIN METHOD REMOVED!", method)
+    await User.update(user, userRow => ({
+      loginMethods: userRow('loginMethods').filter(
+          m => m('id').ne(method.id).or(m('type').ne(method.type))
+      )
+    }))
+  }
+})
+
+module.exports = users
+
+async function start() {
+  rtcms.processServiceDefinition(users, [ ...rtcms.defaultProcessors ])
+  //console.log(JSON.stringify(users.toJSON(), null, "  "))
+  await rtcms.updateService(users)//, { force: true })
+  const service = await rtcms.startService(users, { runCommands: true, handleEvents: true })
+}
+
+if (require.main === module) start().catch( error => { console.error(error); process.exit(1) })
---
+++ tests/tests.js
@@ -0,0 +1,187 @@
+const test = require('blue-tape')
+const r = require('rethinkdb')
+const testUtils = require('rethink-event-sourcing/tape-test-utils.js')
+const crypto = require('crypto')
+
+test('User service', t => {
+  t.plan(7)
+
+  let conn
+
+  testUtils.connectToDatabase(t, r, (connection) => conn = connection)
+
+  let userId
+
+  t.test('create user', t => {
+    t.plan(2)
+
+    testUtils.runCommand(t, r, 'users', {
+      type: 'UserCreate'
+    }, (cId) => { }).then(
+      result => {
+        userId = result
+      }
+    )
+
+    t.test('check if user exists', t=> {
+      t.plan(2)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          userRow => {
+            if(userRow) t.pass('user exists')
+              else t.fail('user not found')
+            t.equals(userRow.display, 'unknown', 'user display name unknown')
+          }
+        ).catch(t.fail)
+      }, 450)
+    })
+
+  })
+
+  let email = "lmTestEmail_" + (Math.random() * 1000000) + "@test.com"
+
+  t.test('add email login method', t => {
+    t.plan(3)
+
+    testUtils.pushEvents(t, r, 'users', [
+      {
+        type: "loginMethodAdded",
+        user: userId,
+        method: {
+          type: 'emailPassword',
+          email: email,
+          id: email
+        }
+      }
+    ])
+
+    t.test('check if method is added', t=> {
+      t.plan(5)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          user => {
+            t.pass('user exists')
+            t.equal(user.loginMethods.length, 1, 'one login method found')
+            t.equal(user.loginMethods[0].type, 'emailPassword', 'one login method type match')
+            t.equal(user.loginMethods[0].id, email, 'one login method id match')
+            t.equal(user.loginMethods[0].email, email, 'one login extra data match')
+          }
+        ).catch(t.fail)
+      }, 250)
+    })
+
+    t.test('check if display name is changed', t=> {
+      t.plan(1)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          user => {
+            t.equal(user.display, email, 'display name generated correctly')
+          }
+        ).catch(t.fail)
+      }, 250)
+    })
+
+  })
+
+  t.test('remove email login method', t => {
+    t.plan(3)
+
+    testUtils.pushEvents(t, r, 'users', [
+      {
+        type: "loginMethodRemoved",
+        user: userId,
+        method: {
+          type: 'emailPassword',
+          email: email,
+          id: email
+        }
+      }
+    ])
+
+    t.test('check if method is removed', t=> {
+      t.plan(2)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          user => {
+            t.pass('user exists')
+            t.equal(user.loginMethods.length, 0, 'zero login methods found')
+          }
+        ).catch(t.fail)
+      }, 250)
+    })
+
+    t.test('check if display name is changed', t=> {
+      t.plan(1)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          user => {
+            t.equal(user.display, 'unknown', 'display name generated correctly')
+          }
+        ).catch(t.fail)
+      }, 250)
+    })
+
+  })
+
+  t.test('update user', t => {
+    t.plan(2)
+
+    testUtils.runCommand(t, r, 'users', {
+      type: 'UserUpdate',
+      parameters: {
+        user: userId,
+        roles: ['test_object']
+      }
+    }, (cId) => { }).then(
+      result => {
+      }
+    )
+
+    t.test('check if user roles is changed', t => {
+      t.plan(1)
+      setTimeout( () => {
+        r.table('users_User').get(userId).run(conn).then(
+          user => {
+            t.deepEqual(user.roles, [ 'test_object' ], 'roles are updated correctly')
+          }
+        ).catch( t.fail )
+      }, 250)
+    })
+
+  })
+
+  t.test('remove user', t => {
+    t.plan(2)
+
+    testUtils.runCommand(t, r, 'users', {
+      type: 'UserDelete',
+      parameters: {
+        user: userId
+      }
+    }, (cId) => { }).then(
+      result => {
+      }
+    )
+
+    t.test('check if user not exists', t=> {
+      t.plan(1)
+      setTimeout(()=>{
+        r.table('users_User').get(userId).run(conn).then(
+          userRow => {
+            if(!userRow) t.pass('user not exists')
+              else t.fail('user still exists')
+          }
+        ).catch(t.fail)
+      }, 250)
+    })
+
+  })
+
+  t.test('close connection', t => {
+    conn.close(() => {
+      t.pass('closed')
+      t.end()
+    })
+  })
+
+})
